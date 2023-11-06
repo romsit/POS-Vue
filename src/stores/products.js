d938396cdf33d0557d2f992d7b56dff99557ef7b
Romina <rominahelou@gmail.com>
2023-11-06 18:31:43 +0100

    Producto anadido

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,7 +1,11 @@
 import { computed } from "vue";
 import { defineStore } from "pinia";
+import { useFirestore } from 'vuefire'
+import { collection, addDoc } from 'firebase/firestore'
 
 export const useProductsStore = defineStore("products", () => {
+
+    const db = useFirestore()
   const categories = [
     { id: 1, name: "Sudaderas" },
     { id: 1, name: "Zapatillas" },
@@ -9,7 +13,7 @@ export const useProductsStore = defineStore("products", () => {
   ];
 
   async function createProduct(product) {
-    console.log(product);
+    await addDoc(collection(db, 'products'), product)
   }
 
   const categoryOptions = computed(() => {
--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
 import Link from '../../components/Link.vue';
 import useImage from '../../composables/useImage'
 import { useProductsStore } from '../../stores/products'
 
 const { url, onFileChange, isImageUploaded } = useImage()
 const products = useProductsStore()
+const router = useRouter()
 
 const formData = reactive({
     name: '',
@@ -22,6 +24,7 @@ const submitHandler = async data => {
             ...values,
             image: url.value
         })
+        router.push({name: 'products'})
     } catch (error) {
         console.log(error)
     }
